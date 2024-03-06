36fa6b29003cf6305ac4931b098dbfeca20c0c61
Nicole <nicolebo@umich.edu>
2024-03-06 14:41:44 -0600

    dev updates to service

--- service-scripts/App-SARS2Wastewater.pl
+++ service-scripts/App-SARS2Wastewater.pl
@@ -1,7 +1,17 @@
-### ### ### ### ### ### edits to barcodes filepath once it is the correct location ### ### ### ### ### ###
+### Before deploying on alpha:
+### Ask bob for help with:
+### change to config write out for sample level metadata (primers and dates)
+### update barcodes path to be a variable?
+### Nicole changes:
+### change job_failed_exists and  save_output_files so it uploads everything
+### change jobs_failed.txt to 'job_summary.txt' and only use job_failed.txt if all samples fail
+### add samtools flag stats
+### add script to make job summary table (that made the 100 samples table)
+###
+
 #
 # App wrapper for the SARS2Waterwater analysis pipeline.
-# This runs snakemake files for sars2-onecodex and Freyja
+# This runs snakemake files for the BV-BRC's sars2-onecodex pipeline and Freyja
 #
 
 use Bio::KBase::AppService::AppScript;
@@ -27,6 +37,28 @@ sub run_app
     my($app, $app_def, $raw_params, $params) = @_;
 	process_read_input($app, $params);
 }
+
+sub job_failed_exists {
+    my($app, $output) = @_;
+    my $job_failed_txt = shift;
+
+    if (-e $job_failed_txt) {
+        print "File '$job_failed_txt' exists. Uploading to workspace \n";
+         my @cmd = ("p3-cp", "ws:" . $app->result_folder);
+	    print STDERR "saving files to workspace... @cmd\n";
+	    my $ok = IPC::Run::run(\@cmd);
+	    if (!$ok)
+	    {
+		warn "Error $? copying output with @cmd\n";
+	    }
+        return 1;  # File exists
+    } else {
+        print "File '$job_failed_txt' does not exist. Uploading all results in the output directory \n";
+        save_output_files($app, $output);
+        return 0;  # File does not exist
+    }
+}
+
 
 sub process_read_input
 {
@@ -74,6 +106,30 @@ sub process_read_input
 			     );
     $params->{$_} = $nparams->{$_} foreach keys %$nparams;
 
+    # If there is  a sample metadata file given copy it into the disk
+    # Access the value for "test_var_1"
+    my $sample_metadata_path = $params->{"sample_metadata_csv"};
+    my $staging_sample_metadata_path = "0";
+    # If a path is provided copy the file to disc and update path 
+    if ($sample_metadata_path eq "0") {
+        print "sample metadata path is equal to 0\n";
+
+    } else {
+        print "sample metadata is is provided\n copying to disc";
+        my @cmd = ("p3-cp","ws:$sample_metadata_path", "$staging");
+	    print STDERR "copying sample metadata file to staging dir @cmd\n";
+	    my $ok = IPC::Run::run(\@cmd);
+	    if (!$ok)
+	    {
+		warn "Error $? copying metadata file failed with @cmd\n";
+	    }
+        if ($sample_metadata_path =~ m|/([^/]+)\.csv$|) {
+            $staging_sample_metadata_path = "$staging/$1.csv";
+        } else {
+            print "No match found\n";
+        }
+    }
+
     print STDERR "Starting the config json....\n";
     my $json_string = encode_json($params);
 
@@ -89,7 +145,8 @@ sub process_read_input
 
     my %config_vars;
     # temp barcode path
-    my $barcodes_path = "/vol/idph/example/usher_barcodes.csv";
+    my $barcodes_path = "/vol/bvbrc/production/application-backend/SARS2WasteWater/2024-03-06/usher_barcodes.csv";
+    my $curated_linages = "/vol/bvbrc/production/application-backend/SARS2WasteWater/2024-03-06/curated_lineages.json";
     my $wf_dir = "$ENV{KB_TOP}/workflows/$ENV{KB_MODULE_DIR}";
     if (! -d $wf_dir)
     {
@@ -103,6 +160,8 @@ sub process_read_input
     #
     my $snakemake = "$ENV{KB_RUNTIME}/artic-ncov2019/bin/snakemake";
     $config_vars{barcodes_path} = $barcodes_path;
+    $config_vars{curated_linages} = $curated_linages;
+    $config_vars{staging_sample_metadata_path} = $staging_sample_metadata_path;
     $config_vars{workflow_dir} = $wf_dir;
     $config_vars{input_data_dir} = $staging;
     $config_vars{output_data_dir} = $output;
@@ -124,7 +183,9 @@ sub process_read_input
     {
      die "wrapper command failed $?: @cmd";
     }
-
+    # check if Job Failed txt file exisits
+    my $filename = "JobFailed.txt";
+    job_failed_exists($filename, $app, $output);
     save_output_files($app, $output);
 }
 
--- workflow/snakefile/wrapper.py
+++ workflow/snakefile/wrapper.py
@@ -13,7 +13,13 @@ import subprocess
 # is designed to be executed by App-SARS2Wastewater perl script
 #
 
-# def check_input_fastqs(input_dir, filename, files_to_zip):
+def check_for_error_msgs(raw_msg, file, out_msg):
+    with open(file) as f:
+        if raw_msg in line:
+            write_to_job_failed_file(out_msg)
+        else:
+            pass
+
 def check_input_fastqs(input_dir, filename):
     input_path = f"{input_dir}/{filename}"
     if os.path.isfile(input_path) == True:
@@ -25,15 +31,14 @@ def check_input_fastqs(input_dir, filename):
         pass
 
 
-def check_sample_metadata_csv(input_info, config_file, input_dir, input_dict):
-    sample_metadata_csv_path = input_dict["sample_metadata_csv"]
-    if sample_metadata_csv_path != "0":
-        if os.path.isfile(sample_metadata_csv_path) == True:
-            metadata_df = pd.read_csv(sample_metadata_csv_path)
-            #edited_sample_metadata_csv_path = ""
+def check_sample_metadata_csv(input_info, config_file, input_dir, staging_metadata_file):
+    if staging_metadata_file != "0":
+        if os.path.isfile(staging_metadata_file) == True:
+            metadata_df = pd.read_csv(staging_metadata_file)
+            #edited_staging_metadata_file = ""
             # check if the date column and sample column are the same length
             if len(metadata_df["Read_file"]) != len(metadata_df["sample_collection_datetime"]):
-                msg = f"Error {sample_metadata_csv_path} is missing a filename or date'. \n check on {sample_metadata_csv_path} \n"
+                msg = f"Error {staging_metadata_file} is missing a filename or date'. \n check on {staging_metadata_file} \n"
                 sys.stderr.write(msg)
             else:
                 ### rewrite the file with <sample_id>.tsv as required by Freyja ###
@@ -45,10 +50,10 @@ def check_sample_metadata_csv(input_info, config_file, input_dir, input_dict):
                 metadata_df = metadata_df[["Sample", "Read_file", "sample_collection_datetime"]]
                 # drop the additional 'Read_file' column no longer needed 
                 metadata_df.drop(columns=["Read_file"], inplace=True)
-                edited_sample_metadata_csv_path = os.path.join(input_dir, "formatted_sample-metadata.csv" )
+                edited_sample_metadata_csv_path = os.path.join(input_dir, "formatted_sample-metadata.csv")
                 metadata_df.to_csv(edited_sample_metadata_csv_path, index=False)
         else:
-            msg = f"Error sample metadata CSV {sample_metadata_csv_path} does not exisit'. \n check on {sample_metadata_csv_path} \n"
+            msg = f"Error sample metadata CSV {staging_metadata_file} does not exisit'. \n check on {staging_metadata_file} \n"
             sys.stderr.write(msg)
             sys.exit(1)
     else:
@@ -76,8 +81,11 @@ def post_processing_check(all_sample_ids, output_dir):
     dict_samples = {}
     complete = []
     incomplete = []
-    for sample_name in all_sample_ids:
-        # check for missing Freyja results
+    #missing_sample_plot
+    # missing_demix
+    for sample_name in all_sample_ids: 
+        # missing demix results
+        # check for missing demix results
         freyja_path = f"{output_dir}/{sample_name}/{sample_name}_demixing_result.csv"
         if os.path.isfile(freyja_path) == True:
             msg = f"{sample_name}"
@@ -86,11 +94,44 @@ def post_processing_check(all_sample_ids, output_dir):
             dict_samples[sample_name] = False
             msg = f"{sample_name}"
             incomplete.append(msg)
+        sample_plot = 
     ## write the complete and incomplete samples to the error report for user to see ##
     if len(incomplete) != 0:
         msg = f"Freyja results produced for the following samples: {complete}. \n \
-                Freyja results missing: Review the following samples: **{incomplete}** \n"
-        sys.stderr.write(msg)
+                Freyja results missing: Review the following samples: **{incomplete}** \n \n \
+                If you have any questions about this job please go to the jobs page \n \
+                select this job. Then on the green action bar, select 'report issue'  \n \
+                A team member will assist you with the analysis. \n"
+        write_to_job_failed_file(msg)
+        # check for errors then add them to Job Failed text file 
+        for sample in incomplete:
+            ### Check demix output ###
+            raw_msg = "demix: Solver error encountered, most likely due to insufficient sequencing depth."
+            out_msg = f"The Freyja demix command gave this error for {sample}: \n 'demix: Solver error encountered, \
+                most likely due to insufficient sequencing depth.'"
+            demix_error_file = f"tmp/{sample}_demix_error.txt"
+            check_for_error_msgs(raw_msg, demix_error_file, out_msg)
+            ### Check sample plots ##
+            # variant plot
+            raw_msg = "ERROR: No samples matching coverage requirements, so no plot will be generated."
+            out_msg = "The Freyja plot command gave this error for {sample}: \n 'ERROR: No samples matching \
+                coverage requirements, so no plot will be generated.' \
+                The default coverage requirement is 60% \n"
+            sample_plot_error_variant_file = f"tmp/{sample}_sample_variant_plot_error.txt"
+            check_for_error_msgs(raw_msg, sample_plot_error_variant_file, out_msg)
+            # linage plot
+            raw_msg = "ERROR: No samples matching coverage requirements, so no plot will be generated."
+            out_msg = "The Freyja plot command with flag '--lineage' gave this error for {sample}: \n 'ERROR: No samples matching \
+                coverage requirements, so no plot will be generated.' \
+                The default coverage requirement is 60% \n"
+            sample_plot_error_lineage_file = f"tmp/{sample}_sample_lineage_plot_error.txt"
+            # lineage plot coverage depth error
+            # lineage plot too many lineages error
+            raw_msg = "Exception: Too many lineages to show. Use --config to group."
+            out_msg = "The Freyja plot command with flag '--lineage' gave this error for {sample}: \n \
+                'Exception: Too many lineages to show.'"
+            check_for_error_msgs(raw_msg, sample_plot_error_lineage_file, out_msg)
+
         sys.exit(1)
     else:
         msg = f"Freyja results produced for the following samples: {complete}. \n"
@@ -125,23 +166,61 @@ def preprocessing_check(output_dir, input_dict):
     dict_samples = {}
     complete = []
     incomplete = []
+    wrong_sequence_type = []
+
 
     # check if iVar bam exisits for each file 
     for sample_name in all_sample_ids:
         iVar_bam = f"{output_dir}/{sample_name}/assembly/{sample_name}.ivar.bam"
+        stats_path = f"{output_dir}/{sample_name}/assembly/{sample_name}.statistics.tsv"
 
-
+        ### check for incorrect sequencing type ###
+        # if the wrong sequencing type is given the stats file will be empty
+        if os.path.isfile(stats_path) == True:
+            if os.path.getsize(stats_path) == 0:
+                wrong_sequence_type.append(sample_name)
+            else:
+                msg = f"assembly complete for {sample_name}"
+                sys.stderr.write(msg)
+        else:
+            wrong_sequence_type.append(sample_name)
+        ### check for assembly results ###
         if os.path.isfile(iVar_bam) == True:
             complete.append(sample_name)
         else:
             dict_samples[sample_name] = False
             incomplete.append(sample_name)
-    # if an iVAR trimmed bam file does not exist write informative message and stop analysis
-    if len(incomplete) != 0:
-        msg = f"Ending job. Not proceeding with the rest of the analysis due to errors in FASTQ proccessing \n \
+
+    ### Write out messages and warnings to JobFailed.txt ###
+    # if both wrong sequencing type and empty ivar bam file
+    if len(incomplete) != 0 and len(wrong_sequence_type) !=0:
+            msg = f"WARNING: Errors in FASTQ proccessing \n \
+            \n CHECK SEQUENCING TYPE: This service only accepts **AMPLICON** based sequencing. \n \
+            Please check the sequencing type for the following samples: **{wrong_sequence_type}** \n \n"
+            write_to_job_failed_file(msg)
+            msg = f" WARNING: CHECK SAMPLE(S) \n \
                 Primer trimming and variant calling is complete for the following samples: {complete}. \n \
-                Primer trimming and variant calling is INCOMPLETE for the following samples: **{incomplete}** \n"
-        sys.stderr.write(msg)
+                CHECK SAMPLE: Primer trimming and variant calling is INCOMPLETE for the following samples: **{incomplete}** \n \
+                Please review the sample(s) by uploading the FASTQ file(s) to the FastqUtils service.\n \
+                Ending job. Not proceeding with the rest of the analysis due to errors in FASTQ proccessing \n"
+            write_to_job_failed_file(msg)
+    ## if an iVAR trimmed bam file does not exist but statstics.tsv exisits 
+    elif len(incomplete) != 0:
+        msg = f"WARNING: CHECK SAMPLE(S) \n \
+                Primer trimming and variant calling is complete for the following samples: {complete}. \n \
+                CHECK SAMPLE: Primer trimming and variant calling is INCOMPLETE for the following samples: **{incomplete}** \n \
+                Please review the sample(s) by uploading the FASTQ file(s) to the FastqUtils service.\n \
+                Ending job. Not proceeding with the rest of the analysis due to errors in FASTQ proccessing \n"
+        write_to_job_failed_file(msg)
+        sys.exit(1)
+    ## if the statistics file is empty it is probably the wrong sequencing type write inormative message and stop the analysis 
+    elif len(wrong_sequence_type) !=0:
+        msg = f"WARNING: Errors in FASTQ proccessing \n \
+                CHECK SEQUENCING TYPE: \n \
+                \n This service only accepts amplicon based sequencing. \n \
+                Please check the sequencing type for the following samples: **{wrong_sequence_type}** \n \
+                Ending job. Not proceeding with the rest of the analysis due to errors in FASTQ proccessing"
+        write_to_job_failed_file(msg)
         sys.exit(1)
     else:
         msg = "preprocessing complete \n"
@@ -151,9 +230,10 @@ def preprocessing_check(output_dir, input_dict):
 
 def run_snakefiles(input_dict, input_dir, output_dir,  config):
     input_dir = config["input_data_dir"]
-    SNAKEMAKE_PATH = config["snakemake"]
-    SNAKEFILE_DIR = f"{config['workflow_dir']}/snakefile/"
-
+    # SNAKEMAKE_PATH = config["snakemake"]
+    # SNAKEFILE_DIR = f"{config['workflow_dir']}/snakefile/"
+    SNAKEMAKE_PATH = "/opt/patric-common/runtime/artic-ncov2019/bin/snakemake"
+    SNAKEFILE_DIR = "/home/nbowers/bvbrc-dev/dev_container/modules/bvbrc_SARS2WasteWater/workflows/snakefile/"
     common_params = [
         SNAKEMAKE_PATH,
         "--cores", str(config["cores"]),
@@ -165,13 +245,15 @@ def run_snakefiles(input_dict, input_dir, output_dir,  config):
 
     if config["cores"] == 1:
         common_params.append("--debug")
-    
+        msg = "starting paired end reads\n"
+        sys.stderr.write(msg)
     # process any paired reads
     if os.path.exists(f"{input_dir}/pe_reads"):
         SNAKEFILE = os.path.join(SNAKEFILE_DIR, "pe_freyja_snakefile")
         cmd = common_params + ["--snakefile",  SNAKEFILE]
         subprocess.run(cmd)
-
+        msg = "starting single end reads\n"
+        sys.stderr.write(msg)
     # process any single reads
     if os.path.exists(f"{input_dir}/se_reads"):
         SNAKEFILE = os.path.join(SNAKEFILE_DIR, "se_freyja_snakefile")
@@ -179,13 +261,14 @@ def run_snakefiles(input_dict, input_dir, output_dir,  config):
         subprocess.run(cmd)
     # once the snake files complete, check for the iVar bam files
     iVar_check = preprocessing_check(output_dir, input_dict)
-    ### check that the iVar files exist ###
+    ## check that the iVar files exist ###
     file_check = iVar_check[0]
     all_sample_ids = iVar_check[1]
 
     if  file_check == True:
         # if all of the iVar bam files exist then the final step will trigger.
         # paired and single end reads will be processed together by Freyja
+        print('starting freyja command')
         SNAKEFILE = os.path.join(SNAKEFILE_DIR, "freyja_snakefile")
         cmd = common_params + ["--snakefile",  SNAKEFILE]
         subprocess.run(cmd)
@@ -282,6 +365,15 @@ def set_up_sample_dictionary(input_dir, input_dict, output_dir, cores):
     return input_info
 
 
+def write_to_warning_file(message):
+    with open("warning.txt", "a") as warning_file:
+        warning_file.write(message + "\n")
+
+def write_to_job_failed_file(message):
+    with open("JobFailed.txt", "a") as job_failed_file:
+        job_failed_file.write(message + "\n")
+
+
 # run the script from service-script/app_App-SARS2Wastewater.pl
 # It takes a single argument which is the pathname of a config.json file
 # This contains the app parameters in the params slot.
@@ -304,13 +396,13 @@ def main(argv):
     input_dict = config["params"]
     input_dir = config["input_data_dir"]
     output_dir = config["output_data_dir"]
+    staging_metadata_file = config["staging_sample_metadata_path"]
 
     # rename files according to sample ids, copy files to pe_reads and se_reads in the staging directory
     input_info = set_up_sample_dictionary(input_dir, input_dict, output_dir, min(8, int(config["cores"])))
-    # check sample metadata csv
-    check_sample_metadata_csv(input_info, config_file, input_dir, input_dict)
+    # if a user provided metadata csv exists, make sure it matches the reads given
+    check_sample_metadata_csv(input_info, config_file, input_dir, staging_metadata_file)
     # run the snakefiles
     run_snakefiles(input_dict, input_dir, output_dir, config)
-
 if __name__ == "__main__":
     main(sys.argv[1:])
